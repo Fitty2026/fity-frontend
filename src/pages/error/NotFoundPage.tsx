08f411c19f266b49eb46d6b05393aff09504e94d
chaeyeon089 <chaeyeon0091@naver.com>
2026-07-12 02:17:43 +0900

    feat: 라우터 전역 에러 처리 및 404 화면 연결

---
+++ src/pages/error/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import PageLayout from '@/components/layout/PageeLayout';
+import ErrorScreen from '@/components/ui/ErrorScreen';
+
+const NotFoundPage = () => (
+  <PageLayout showHeader={false} showBottomNav={false} className="flex">
+    <ErrorScreen
+      title="페이지를 찾을 수 없어요"
+      description="주소가 잘못되었거나 삭제된 페이지예요"
+    />
+  </PageLayout>
+);
+
+export default NotFoundPage;
