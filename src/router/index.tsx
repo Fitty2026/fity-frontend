6501d96f0def891e353c7b153243a8f3159674ed
이재인 <jilee0818@gmail.com>
2026-08-21 19:41:38 +0900

    feat: 카메라·앨범으로 옷을 추가하는 갈래 연결
    시안이 등록 방식을 4종으로 늘리면서 사진을 직접 올리는 길이 생김. 영수증·구매내역이 상품 수정 화면에서 태그를 고치는 것과 달리, 이쪽은 여러 벌을 한 번에 훑는 확인·수정 화면을 따로 거친다.
    
    - 카메라는 촬영 화면을, 앨범은 파일 선택을 거쳐 태그 확인으로 수렴
    - 카드가 4장이 되어 화면을 넘쳐 문구 기준 배치를 바닥 기준으로 변경
    - 태그 값은 생성 API 대기라 목업이며, 사진만 실제로 올린 것을 사용

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -41,6 +41,8 @@ import ClosetItemDetailPage from '../pages/closet/ClosetItemDetailPage';
 import ClosetItemDeletePage from '../pages/closet/ClosetItemDeletePage';
 import ClosetItemListPage from '../pages/closet/ClosetItemListPage';
 import ClosetIntroPage from '../pages/closet/ClosetIntroPage';
+import ClosetTagConfirmPage from '../pages/closet/ClosetTagConfirmPage';
+import ClosetTagEditPage from '../pages/closet/ClosetTagEditPage';
 
 // 3. 홈
 
@@ -214,6 +216,15 @@ const router = createBrowserRouter([
   {
     path: '/closet/register/photo',
     element: <ProtectedRoute><ClosetPhotoPage /></ProtectedRoute>,
+  },
+  // 카메라·앨범으로 올린 옷 사진의 태그 확인·수정 (영수증·구매내역은 product-images/edit가 맡는다)
+  {
+    path: '/closet/register/tags',
+    element: <ProtectedRoute><ClosetTagConfirmPage /></ProtectedRoute>,
+  },
+  {
+    path: '/closet/register/tags/edit',
+    element: <ProtectedRoute><ClosetTagEditPage /></ProtectedRoute>,
   },
   {
     path: '/closet/register/added',
